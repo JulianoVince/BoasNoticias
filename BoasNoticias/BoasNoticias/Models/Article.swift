16c26ce9a409763680ac4079feba286654f3fefe
Juliano Campos <julianovincedecampos@hotmail.com>
2019-07-17 06:35:17 -0300

    Criando package e Struct/Model de Article

---
+++ BoasNoticias/BoasNoticias/Models/Article.swift
@@ -0,0 +1,16 @@
+//
+//  Article.swift
+//  BoasNoticias
+//
+//  Created by Juliano Campos on 17/07/19.
+//  Copyright © 2019 Juliano Campos. All rights reserved.
+//
+
+import Foundation
+
+struct Article {
+    
+    let title:String
+    let description:String
+    
+}
